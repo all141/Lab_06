340f99b2b244f3ea89e28154399c6bc94ec0427f
all141 <all141@pitt.edu>
2015-11-30 21:50:18 -0500

    Update nullPointer.java

--- nullPointer.java
+++ nullPointer.java
@@ -1,8 +1,14 @@
-public class nPE
+public class Example
 {
     public static void main(String[] args)
     {
-        Object obj = null;
+    	//Here is the code that would give me a null pointer exception
+    	/**
+    	 * Object obj = null;
+         *  obj.hashCode();
+    	 */
+    	 //Here is how I fix it
+        Object obj = new Object();
         obj.hashCode();
     }
 }
