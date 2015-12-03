6cb96977c823b97c08947e7f8cc110a53561b15f
all141 <all141@pitt.edu>
2015-12-02 20:03:41 -0500

    Update noogie.java

--- noogie.java
+++ noogie.java
@@ -1,1 +1,29 @@
+//Throws Exception when variable takes value of a non-perfect square
+class noogieException extends Exception 
+{
+	      //Parameterless Constructor
+	      public noogieException() {}
+
+	      //Constructor that accepts a message
+	      public noogieException(String message)
+	      {
+	         super(message);
+	      }
+	private static final long serialVersionUID = 1L; 
+	public static void main(String[] args) 
+	{
+		String word = null;
+		try 
+		{
+			if(word.contains(" "))
+		     {
+		          throw new noogieException();
+		     }
+		} catch (noogieException e) 
+		{
+			
+		}
+	}
+}
+
 
